e7d8901eda9537c18e11ade3035bda5c5a9ab30c
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2021-04-15 09:44:14 -0300

    Criar projeto

---
+++ src/main/java/wqs/example/concurrency/labconcurrency/LabconcurrencyApplication.java
@@ -0,0 +1,13 @@
+package wqs.example.concurrency.labconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabconcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabconcurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wqs/example/concurrency/labconcurrency/LabconcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package wqs.example.concurrency.labconcurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabconcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
